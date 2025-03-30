5d0b1bd290d1f187c34b973d845f9d8133756580
mrampant <mathieu.rampant@gmail.com>
2025-03-30 14:07:23 -0400

    - adding test for postgres 13 back and removing conn_max_age from tests hoping it will prevent issues when tearing down test db

--- NEMO/tests/test_settings.py
+++ NEMO/tests/test_settings.py
@@ -103,7 +103,6 @@ DATABASES = {
         "PASSWORD": os.getenv("DATABASE_PASSWORD", ""),
         "HOST": os.getenv("DATABASE_HOST", ""),
         "PORT": os.getenv("DATABASE_PORT", ""),
-        "CONN_MAX_AGE": 60,
         "TEST": {
             "NAME": os.getenv("DATABASE_NAME", "./test_nemo.db"),
         },
